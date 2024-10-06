6a8698f67cb4ca435ab9403e74e07f0d0e9c3d8f
ibrahim abdo <ibrahim10abdo@outlook.com>
2024-10-06 13:37:10 +0300

    test item model need to be handled

---
+++ src/POS.API/Tests/Attribute.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.API.Tests;
+
+public class Attribute
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    // Removed RelatedItemId/RelatedItem since Attribute belongs to an Item
+    public ICollection<AttributeItem> AttributeItems { get; set; } = new HashSet<AttributeItem>();
+}
---
+++ src/POS.API/Tests/AttributeItem.cs
@@ -0,0 +1,13 @@
+﻿namespace POS.API.Tests;
+
+public class AttributeItem
+{
+    public int Id { get; set; }
+    public int AppearanceIndex { get; set; }  // Determines the order in which items appear
+
+    public int AttributeId { get; set; }
+    public Attribute Attribute { get; set; }
+
+    public int RelatedItemId { get; set; }  // This links to the related item
+    public Item RelatedItem { get; set; }
+}
