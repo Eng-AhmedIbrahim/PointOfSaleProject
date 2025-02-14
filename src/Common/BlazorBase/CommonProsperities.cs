a94f9ae532dbcbc7e71bf10f78175843efa2aea4
ahmedIbrahim <roma200132@gmail.com>
2025-02-14 16:37:46 +0200

    AddSection3AndEditSettingPage

--- src/Common/BlazorBase/CommonProsperities.cs
+++ src/Common/BlazorBase/CommonProsperities.cs
@@ -5,4 +5,6 @@ public class CommonProsperities
     public double CategorySpacing { get; set; } = 4.0;
     public double CategoryPadding => CategorySpacing * 2;
     public double CategoryFontSize => CategorySpacing + 16;
+    public double _salesItemsHorizontalSider = 4;
+    public double _salesItemsVerticalSlider = 4;
 }
